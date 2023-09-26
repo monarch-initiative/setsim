53ce653f444683e2589eef5309279b7ee1b9ab44
bcoleman96 <coleman.benjamin@gmail.com>
2023-09-26 07:38:05 -0400

    Add code to compute function of SumSimSimilarityKernel.

--- src/sumsim/sim/_sumsim.py
+++ src/sumsim/sim/_sumsim.py
@@ -1,6 +1,7 @@
 import typing
 
 import hpotk
+import hpotk.algorithm
 
 from sumsim.model import Phenotyped
 
@@ -14,7 +15,11 @@ class SumSimSimilarityKernel(SimilarityKernel):
         self._hpo = hpo.graph
         self._delta_ic = delta_ic_dict
 
-
     def compute(self, a: Phenotyped, b: Phenotyped) -> SimilarityResult:
-        # TODO - implement the similarity calculation using the delta IC dict and the ontology graph
-        pass
+        a_features = set(ancestor for pf in a.phenotypic_features for ancestor in
+                         self._hpo.get_ancestors(pf, include_source=True))
+        b_features = set(ancestor for pf in b.phenotypic_features for ancestor in
+                         self._hpo.get_ancestors(pf, include_source=True))
+        ab_features = list(a_features.intersection(b_features))
+        ab_similarity = sum([self._delta_ic[pf] for pf in ab_features])
+        return SimilarityResult(ab_similarity)
