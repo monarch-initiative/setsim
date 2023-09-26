18be7aef8ca459bb609c29802ce45b065d2a51a8
bcoleman96 <coleman.benjamin@gmail.com>
2023-09-26 11:02:36 -0400

    Copied more from c2s2

--- src/sumsim/io.py
+++ src/sumsim/io.py
@@ -49,7 +49,6 @@ def _parse_phenopacket(phenopacket: Phenopacket) -> Sample:
 
     return SimpleSample(identifier, phenotypic_features)
 
-
 def read_cohort(cohort: typing.Union[Cohort, typing.IO, str]) -> typing.Sequence[Sample]:
     """
     Read Cohort into a `sumsim.model.Sample`.
@@ -61,4 +60,51 @@ def read_cohort(cohort: typing.Union[Cohort, typing.IO, str]) -> typing.Sequence
     if not isinstance(cohort, Message):
         cohort = read_protobuf_message(cohort, Cohort())
     return _parse_cohort(cohort)
+
+
+def _parse_cohort(cohort: Cohort) -> typing.Sequence[Sample]:
+    """
+    Extract `sumsim.model.Sample`s from a `Cohort` into a sequence of `sumsim.model.Sample`s.
+    Each cohort member is transformed into one `sumsim.model.Sample`.
+
+    :raises: a `ValueError` if the input is not a `Cohort`.
+    """
+    if not isinstance(cohort, Cohort):
+        raise ValueError(f'Expected an argument with type {Cohort} but got {type(cohort)}')
+    return [_parse_phenopacket(member) for member in cohort.members]
+
+
+def read_protobuf_message(fh: typing.Union[typing.IO, str], message: MESSAGE, encoding: str = 'utf-8') -> MESSAGE:
+    """
+    Read Protobuf data from ``fh`` and store in the ``message``.
+
+    :param fh: a `str` pointing to a file with JSON representation of the ``message`` or the actual JSON `str`
+           or IO wrapper (either text or binary) with the JSON content. File and binary inputs are decoded
+           using provided ``encoding``.
+    :param message: protobuf object to be filled with the input data
+    :param encoding: encoding to parse binary or file inputs.
+    :return:
+    """
+    if isinstance(fh, str):
+        if os.path.exists(fh):
+            if os.path.isfile(fh):
+                logger.debug(f'A file has been provided, decoding using `{encoding}`')
+                with open(fh, 'r', encoding=encoding) as handle:
+                    return read_protobuf_message(handle, message)
+            elif os.path.isdir(fh):
+                raise ValueError(f'Cannot read protobuf message from a directory {fh}. Please provide a file instead')
+            else:
+                raise ValueError(f'Unrecognized `str` input')
+        else:
+            logger.debug(f'Assuming `fh` is a JSON `str` of a top-level phenopacket schema object, '
+                         f'decoding using `{encoding}`')
+            return Parse(fh, message)
+    elif isinstance(fh, io.TextIOBase):
+        return Parse(fh.read(), message)
+    elif isinstance(fh, io.BufferedIOBase):
+        logger.debug(f'Byte IO handle provided, decoding using `{encoding}`')
+        return Parse(fh.read().decode(encoding), message)
+    else:
+        raise ValueError(f'Expected a path to phenopacket JSON, phenopacket JSON `str`, or an IO wrapper '
+                         f'but received {type(fh)}')
 
