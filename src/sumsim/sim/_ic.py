a22c9f4ed4429b7e4b245f14d368845ac3e07109
bcoleman96 <coleman.benjamin@gmail.com>
2023-10-18 20:02:46 -0400

    Make ic_dict use hpotk.TermID instead of string and started building tests.

--- src/sumsim/sim/_ic.py
+++ src/sumsim/sim/_ic.py
@@ -14,9 +14,10 @@ class IcTransformer:
     Transform information contents to delta information contents.
     """
 
-    def __init__(self, hpo: hpotk.MinimalOntology,
+    def __init__(self, hpo: hpotk.MinimalOntology, root: str = "HP:0000118",
                  strategy: str = 'mean'):
         self._hpo = hpo
+        self._root = hpo.get_term(root).identifier
         self._strategy = strategy
 
     def transform(self, ic_dict: typing.Mapping[hpotk.TermId, float]) -> typing.Mapping[hpotk.TermId, float]:
@@ -29,20 +30,20 @@ class IcTransformer:
 
     def _use_mean(self, ic_dict: typing.Mapping[hpotk.TermId, float]) -> typing.Mapping[hpotk.TermId, float]:
         delta_ic_dict = {}
-        for TermID in ic_dict:
-            if TermID == self._hpo.graph.root.value:
+        for term in ic_dict:
+            if term == self._root:
                 parent_ic = 0
             else:
-                parents = self._hpo.graph.get_parents(TermID)
-                parent_ic = mean(ic_dict[i.value] for i in parents)
-            delta_ic_dict[TermID] = ic_dict[TermID] - parent_ic
+                parents = self._hpo.graph.get_parents(term)
+                parent_ic = mean(ic_dict[i] for i in parents)
+            delta_ic_dict[term] = ic_dict[term] - parent_ic
         return delta_ic_dict
 
     def _use_max(self, ic_dict: typing.Mapping[hpotk.TermId, float]) -> typing.Mapping[hpotk.TermId, float]:
         delta_ic_dict = {}
-        for TermID in ic_dict:
-            parent_ic = max([ic_dict[i.value] for i in self._hpo.graph.get_parents(TermID)], default=0)
-            delta_ic_dict[TermID] = ic_dict[TermID] - parent_ic
+        for term in ic_dict:
+            parent_ic = max([ic_dict[i] for i in self._hpo.graph.get_parents(term)], default=0)
+            delta_ic_dict[term] = ic_dict[term] - parent_ic
         return delta_ic_dict
 
 
@@ -51,28 +52,31 @@ class IcCalculator:
     Create a dictionary providing the information content of terms.
     """
 
-    def __init__(self, hpo: hpotk.MinimalOntology):
+    def __init__(self, hpo: hpotk.MinimalOntology, root: str = "HP:0000118"):
         self._hpo = hpo.graph
+        self._root = hpo.get_term(root)
         self.samples = None
         self.used_terms = set()
         self.sample_array = None
 
-    def calculate_ic_from_samples(self, samples: typing.Sequence[Sample], root: str = "HP:0000001") -> typing.Mapping[hpotk.TermId, float]:
+    def calculate_ic_from_samples(self, samples: typing.Sequence[Sample]) -> typing.Mapping[
+        hpotk.TermId, float]:
         self.samples = samples
-        self.used_terms = set(pf.value for sample in samples for pf in sample.phenotypic_features)
+        self.used_terms = set(pf for sample in samples for pf in sample.phenotypic_features)
         self.sample_array = self._get_sample_array()
         # Define the number of processes to use
         num_processes = multiprocessing.cpu_count() - 2  # Use all but 2 available CPU cores
         # Create a multiprocessing pool
         pool = multiprocessing.Pool(processes=num_processes)
-        results = list(pool.imap(self._get_term_freq, self._hpo.get_descendants(root, include_source=True)))
+        results = list(pool.imap(self._get_term_freq, self._hpo.get_descendants(self._root, include_source=True)))
         pool.close()
         ic_dict = {key: value for key, value in results}
         return ic_dict
 
     def _get_sample_array(self) -> np.array:
-        dtype = [(col, bool) for col in self.used_terms]
-        array = np.zeros(len(self.samples), dtype=dtype)
+        # Convert hpotk.TermID to string for array index
+        array_type = [(col.value, bool) for col in self.used_terms]
+        array = np.zeros(len(self.samples), dtype=array_type)
         i = 0
         for sample in self.samples:
             for term in sample.phenotypic_features:
@@ -80,14 +84,12 @@ class IcCalculator:
             i = i + 1
         return array
 
-
-    def _get_term_freq(self, term: hpotk.TermId) -> (str, float):
-        term_descendants = set(i.value for i in self._hpo.get_descendants(term.value, include_source=True))
-        relevant_descendants = list(term_descendants & self.used_terms)
+    def _get_term_freq(self, term: hpotk.TermId) -> (hpotk.TermId, float):
+        term_descendants = set(i for i in self._hpo.get_descendants(term, include_source=True))
+        relevant_descendants = [i.value for i in list(term_descendants & self.used_terms)]
         if len(relevant_descendants) > 0:
             freq = sum(1 if any(row[relevant_descendants]) else 0 for row in self.sample_array)
         else:
             freq = 1
-        ic = log(len(self.sample_array)/freq)
-        return (term.value, ic)
-
+        ic = log(len(self.sample_array) / freq)
+        return term, ic
