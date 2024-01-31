bb3c46cebc4639302fbdcca6bbf9c2a69b4f5a45
bcoleman96 <coleman.benjamin@gmail.com>
2024-01-31 09:58:36 -0500

    Added simgic and began modifying structure of similarity functions

--- src/sumsim/sim/_sumsim.py
+++ src/sumsim/sim/_sumsim.py
@@ -22,10 +22,13 @@ class SumSimilarity(SetSimilarityKernel, metaclass=abc.ABCMeta):
 
     def _score_feature_sets(self, feature_sets: (typing.Set[hpotk.TermId], typing.Set[hpotk.TermId])) -> float:
         all_features = feature_sets[0].intersection(feature_sets[1])
+        return self._score_feature_set(all_features)
+
+    def _score_feature_set(self, feature_set: typing.Set[hpotk.TermId]) -> float:
         try:
-            sim = sum(self._delta_ic_dict.get(term, None) for term in all_features)
+            sim = sum(self._delta_ic_dict.get(term, None) for term in feature_set)
         except KeyError:
-            features = [feature for feature in all_features if self._delta_ic_dict.get(feature, None) is None]
+            features = [feature for feature in feature_set if self._delta_ic_dict.get(feature, None) is None]
             print(f'Samples share the following features which are not included in the provided dictionary:'
                   f'\n{features}')
             raise
@@ -44,30 +47,20 @@ class SumSimSimilarityKernel(OntoSetSimilarityKernel, SumSimilarity):
         return True
 
 
-class SumSimSimilaritiesKernel(SetSimilaritiesKernel, metaclass=abc.ABCMeta):
+class SumSimSimilaritiesKernel(SetSimilaritiesKernel, SumSimilarity, metaclass=abc.ABCMeta):
     def __init__(self, disease: Phenotyped, hpo: hpotk.GraphAware, delta_ic_dict: typing.Mapping[hpotk.TermId, float],
-                 root: str = "HP:0000118", return_last_result: bool = False):
+                 root: str = "HP:0000118"):
         SetSimilaritiesKernel.__init__(self, disease, hpo, root)
+        SumSimilarity.__init__(self, delta_ic_dict)
         self._delta_ic_dict = delta_ic_dict
-
-    def _score_addition(self, new_feature_set: Set[hpotk.TermId], disease_leftovers: Set[hpotk.TermId]) \
-            -> (float, Set[hpotk.TermId]):
-        all_features = new_feature_set.intersection(disease_leftovers)
-        try:
-            sim = sum(self._delta_ic_dict.get(term, None) for term in all_features)
-        except KeyError:
-            features = [feature for feature in all_features if self._delta_ic_dict.get(feature, None) is None]
-            print(f'Samples share the following features which are not included in the provided dictionary:'
-                  f'\n{features}')
-            raise
-        return sim, disease_leftovers.difference(all_features)
 
     def compute(self, sample: Phenotyped, return_last_result: bool = False) -> typing.Union[typing.Sequence[float], float]:
         disease_leftovers = self._disease_features.copy()
         sim = 0.0
         results = []
         for next_set in self._sample_iterator(sample):
-            sim_addition, disease_leftovers = self._score_addition(next_set, disease_leftovers)
+            sim_addition = self._score_feature_sets((next_set, disease_leftovers))
+            disease_leftovers = disease_leftovers.difference(next_set)
             sim += sim_addition
             results.append(sim)
         if return_last_result:
